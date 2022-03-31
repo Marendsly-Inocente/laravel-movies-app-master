3872804a16eff6b8f658f45aade82434aaf68f59
Marendsly <509972@vistacollege.nl>
2022-03-31 17:25:32 +0200

    marendsly-version

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\MoviesController;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
+
+
+// Route::view('/', 'index');
+// Route::view('/movie', 'show');
